937f91deda780dc706b8d6f60922bf6c0f52045c
Aditya Patil <adityasp2308@gmail.com>
2024-12-29 22:42:50 +0530

    Added Landingpage.js

--- gdgzcoer/src/app/components/Homepage.js
+++ gdgzcoer/src/app/components/Homepage.js
@@ -1,12 +1,14 @@
 import Footer from "./Footer";
 import Form from "./Form";
 import HeroAbout from "./HeroAbout";
+import Landingpage from "./Landingpage";
 import Navbar from "./Navbar";
 import Vision from "./VisionCards";
 export default function Homepage(){
     return(
         <div>
             <Navbar/>
+            <Landingpage/>
             <HeroAbout/>
             
             <Vision/>
--- gdgzcoer/src/app/page.js
+++ gdgzcoer/src/app/page.js
@@ -1,4 +1,5 @@
 import Homepage from "./components/Homepage";
+import Landingpage from "./components/Landingpage";
 
 export default function Home() {
   return (
