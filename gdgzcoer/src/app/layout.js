b722ef9cb47798bd41c9298d2fea5de49f269ceb
Aditya Patil <adityasp2308@gmail.com>
2024-12-29 13:22:08 +0530

    Added Landingpage.js

--- gdgzcoer/src/app/layout.js
+++ gdgzcoer/src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         {children}
       </body>
